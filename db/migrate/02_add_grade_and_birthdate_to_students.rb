1f2fcf76e789d86b4c53557ef61b048f4f8e8b4c
Laura Messa Travin <laura.travin@gmail.com>
2018-11-13 20:37:45 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddFColumns < ActiveRecord::Migration
+class  AddGradeAndBirthdateToStudents < ActiveRecord::Migration
  def change
    add_column :students, :grade, :interger
    add_column :students, :birthdate, :string
